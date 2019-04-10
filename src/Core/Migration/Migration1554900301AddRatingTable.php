2d913586765301beeb2217e6557be38d06b27831
st.hamann <sth@shopware.de>
2019-04-10 14:49:23 +0200

    Add Migration for rating

---
+++ src/Core/Migration/Migration1554900301AddRatingTable.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1554900301AddRatingTable extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1554900301;
+    }
+
+    public function update(Connection $connection): void
+    {
+        // implement update
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
