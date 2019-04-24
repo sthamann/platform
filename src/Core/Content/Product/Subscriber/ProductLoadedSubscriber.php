75fadadbcee42e7b3e05d67b49387ee3ad44473a
Stefan Hamann <st.hamann@shopware.com>
2019-04-24 21:01:00 +0200

    Add rating matrix - update project status (openissues.md)

--- src/Core/Content/Product/Subscriber/ProductLoadedSubscriber.php
+++ src/Core/Content/Product/Subscriber/ProductLoadedSubscriber.php
@@ -7,6 +7,10 @@ use Shopware\Core\Content\Product\ProductEvents;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Class ProductLoadedSubscriber
+ * @package Shopware\Core\Content\Product\Subscriber
+ */
 class ProductLoadedSubscriber implements EventSubscriberInterface
 {
     public static function getSubscribedEvents(): array
@@ -20,18 +24,22 @@ class ProductLoadedSubscriber implements EventSubscriberInterface
 
     public function unserialize(EntityLoadedEvent $event): void
     {
+        // AHRG!!!! TODO AT STH // IST DAS HIER RICHTIG ???
         /** @var ProductEntity $product */
         foreach ($event->getEntities() as $product) {
             $reviews = $product->getReviews();
             $avgPoints = 0;
+            $ratingMatrix = array('0' => 0, '1' => 0,'2' => 0, '3' => 0,'4' => 0,'5' => 0);
             if ($reviews !== null){
                 foreach ($reviews as $review){
                     $avgPoints += $review->getPoints();
+                    $ratingMatrix[$review->getPoints()]++;
                 }
                 $avgPoints = $avgPoints / count($product->getReviews());
             }
 
             $product->ratingAvg = $avgPoints;
+            $product->ratingMatrix = $ratingMatrix;
         }
     }
 }
